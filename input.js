dbb9de89f44e0d987718a8ed476511ec3168f8ae
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-11-04 04:04:20 +0000

    caht bubbles

--- input.js
+++ input.js
@@ -1,7 +1,7 @@
 // const { connect } = require("./client");
 // let conn = connect();
 const handleUserInput = function (keyStroke) {
-  if (keyStroke === '\u0003' ){
+  if (keyStroke === '\u0003') {
     process.exit();
   }
 };
@@ -14,29 +14,33 @@ const setupInput = function (conn) {
   stdin.resume();
 
   stdin.on("data", handleUserInput);
-  stdin.on('data', (keyStroke)=> { 
-    if( keyStroke === 'w') {
-    keyStroke = "Move: up"  
-    connection.write(keyStroke);
-    }
-    if( keyStroke === 'a') {
-      keyStroke = "Move: left"  
+  stdin.on('data', (keyStroke) => {
+    if (keyStroke === 'w') {
+      keyStroke = "Move: up"
       connection.write(keyStroke);
-      }
-    if( keyStroke === 's') {
-        keyStroke = "Move: down"  
-        connection.write(keyStroke);
-        }
-    if( keyStroke === 'd') {
-          keyStroke = "Move: right"  
-          connection.write(keyStroke);
-          }
-          
-        
-      
+    }
+    if (keyStroke === 'a') {
+      keyStroke = "Move: left"
+      connection.write(keyStroke);
+    }
+    if (keyStroke === 's') {
+      keyStroke = "Move: down"
+      connection.write(keyStroke);
+    }
+    if (keyStroke === 'd') {
+      keyStroke = "Move: right"
+      connection.write(keyStroke);
+    }
+    if (keyStroke === '1') {
+      keyStroke = "Say: gotcha!"
+      connection.write(keyStroke);
+    }
+    if (keyStroke === '2') {
+      keyStroke = "Say: run for ur life!"
+      connection.write(keyStroke);
+    }
 
-
-  }) 
+  })
   return stdin;
 };
 
