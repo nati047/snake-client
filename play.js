37594aa48450fd621affc732a61f5e61655d09df
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-11-04 01:28:28 +0000

    stdin setup

--- play.js
+++ play.js
@@ -6,12 +6,36 @@ console.log("Connecting ...");
 
 conn.on('connect',(user) => {
   conn.write( 'Name: NAT')  
- 
-    // conn.write( 'Move: up')
-
- 
+     // conn.write( 'Move: up')
 });
 
-
-
 conn.on('data',(data) => console.log( data)  );
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+const input = setupInput();
+
+const handleUserInput = function (keyStroke) {
+  if (keyStroke === '\u0003' ){
+    process.exit();
+  }
+};
+input.on('data',handleUserInput);
+
+
+// (data)=> {
+//   if( data === '\u0003')
+//     process.exit();
+// }
+
+// stdin.on("data", handleUserInput);
+
+
+//'\u0003'
