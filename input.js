5bb14d5071d6747c2a89c38b5a4f23ba6598a092
Natnael Tekletsadik <isaacnatnael@gmail.com>
2021-11-04 03:58:40 +0000

    snake movement setup

---
+++ input.js
@@ -0,0 +1,44 @@
+// const { connect } = require("./client");
+// let conn = connect();
+const handleUserInput = function (keyStroke) {
+  if (keyStroke === '\u0003' ){
+    process.exit();
+  }
+};
+// setup interface to handle user input from stdin
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+  stdin.on('data', (keyStroke)=> { 
+    if( keyStroke === 'w') {
+    keyStroke = "Move: up"  
+    connection.write(keyStroke);
+    }
+    if( keyStroke === 'a') {
+      keyStroke = "Move: left"  
+      connection.write(keyStroke);
+      }
+    if( keyStroke === 's') {
+        keyStroke = "Move: down"  
+        connection.write(keyStroke);
+        }
+    if( keyStroke === 'd') {
+          keyStroke = "Move: right"  
+          connection.write(keyStroke);
+          }
+          
+        
+      
+
+
+  }) 
+  return stdin;
+};
+
+module.exports = { setupInput }
+
--- play.js
+++ play.js
@@ -1,41 +1,29 @@
-const connect = require('./client');
-
-const conn = connect();
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
+const conn = connect();
+
+setupInput(conn);
+
+
+
+// const connect = require('./client');
+
+// const conn = connect();
+
+// console.log("Connecting ...");
 
 conn.on('connect',(user) => {
   conn.write( 'Name: NAT')  
-     // conn.write( 'Move: up')
+     
 });
 
-conn.on('data',(data) => console.log( data)  );
-
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-const input = setupInput();
-
-const handleUserInput = function (keyStroke) {
-  if (keyStroke === '\u0003' ){
-    process.exit();
-  }
-};
-input.on('data',handleUserInput);
+// conn.on('data',(data) => console.log( data)  );
 
 
-// (data)=> {
-//   if( data === '\u0003')
-//     process.exit();
-// }
-
-// stdin.on("data", handleUserInput);
 
 
-//'\u0003'
+
+
+
